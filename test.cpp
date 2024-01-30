6fa67ecdbb5acf137ea04b3788c24c1665c98f32
Laxman Seelan <laxman.seelan@student.manchester.ac.uk>
2024-01-30 02:59:04 +0000

    2nd commit

--- test.cpp
+++ test.cpp
@@ -1,1 +1,23 @@
-// Test
+// Assignment 1 - skeleton code
+
+// Program to calculate transition energy using simple Bohr formula
+
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+  // Declare variables here
+  float initial_principle_quantum_number
+
+  // Ask user to enter atomic number
+  std::cout << "Hello, World!" << std::endl;
+
+  // Ask user to enter initial and final quantum numbers
+
+  // Compute photon energy, Delta E = 13.6*(Z^2)*(1/n_j^2-1/n_i^2) eV
+
+  // Output answer
+
+  return 0;
+}
